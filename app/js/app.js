f8020a7aa4638244ef040a97a9bde8c59070f9ae
Brian Mosley & Raymond Sapida <pair+brianlmosley.raysapida@devbootcamp.com>
2016-06-21 17:45:37 -0700

    Add ajax post method to add badges to the teacher show page

--- app/js/app.js
+++ app/js/app.js
@@ -1,34 +1,7 @@
 $.ready(function () {
   var teachers;
   setUpIndexPage();
-
-  window.addEventListener('hashchange', function(){
-    if (!location.toString().match(/\#/)) {
-      miniQuery('#index').show();
-      miniQuery('#show').hide();
-    } else {
-      miniQuery('#index').hide();
-      miniQuery('#show').show();
-      var name = location.toString().split('#')[1]
-      var teacher_id = findTeacher(name)[0].id;
-      console.log(teacher_id);
-
-      $.ajax({
-        url: "http://spa-badge-api.herokuapp.com/teachers/" + teacher_id,
-        type: "GET"
-      }).then(function(response){
-       var teacher = JSON.parse(response);
-       var theTemplateScript = miniQuery("#teacher-badge-template").select()[0].innerHTML;
-       var theTemplate = Handlebars.compile(theTemplateScript);
-       var theCompiledHtml = theTemplate(teacher);
-       miniQuery('#show').select()[0].innerHTML = theCompiledHtml;
-      })
-
-    }
-
-  })
-
-
+  listenForHashChanges();
 });
 
 
@@ -56,4 +29,55 @@ function showTeacherList(teachers) {
   var context = { teachers: teachers}
   var theCompiledHtml = theTemplate(context);
   miniQuery('.teachers-placeholder').select()[0].innerHTML = theCompiledHtml;
+}
+
+function listenForHashChanges(argument) {
+  window.addEventListener('hashchange', function(){
+    if (!location.toString().match(/\#/)) {
+      miniQuery('#index').show();
+      miniQuery('#show').hide();
+    } else {
+      miniQuery('#index').hide();
+      miniQuery('#show').show();
+      var name = location.toString().split('#')[1]
+      var teacher_id = findTeacher(name)[0].id;
+
+      $.ajax({
+        url: "http://spa-badge-api.herokuapp.com/teachers/" + teacher_id,
+        type: "GET"
+      }).then(function(response){
+         var teacher = JSON.parse(response);
+         var theTemplateScript = miniQuery("#teacher-badge-template").select()[0].innerHTML;
+         var theTemplate = Handlebars.compile(theTemplateScript);
+         var theCompiledHtml = theTemplate(teacher);
+         miniQuery('#show').select()[0].innerHTML = theCompiledHtml;
+
+         miniQuery('.submit-form').on('submit', function(event){
+          event.preventDefault();
+          var url = this.getAttribute("action");
+          var inputs = this.getElementsByTagName('input')
+          var phrase = inputs.phrase.value;
+          var teacher_id = inputs.teacher_id.value;
+          var votes = inputs.votes.value;
+
+          $.ajax({
+            url: url,
+            type: 'POST',
+            data: "phrase="+phrase+"&teacher_id="+teacher_id+"&votes="+votes
+          }).then(function(response) {
+
+            var badge = JSON.parse(response);
+            console.log(badge);
+            var theTemplateScript = miniQuery("#badge-template").select()[0].innerHTML;
+            var theTemplate = Handlebars.compile(theTemplateScript);
+            var theCompiledHtml = theTemplate(badge);
+            console.log(miniQuery('.show-user').select()[0].outerHTML);
+            miniQuery('.show-user').select()[0].innerHTML += theCompiledHtml;
+          }).catch(function(response) {
+            console.log(response);
+          })
+        })
+      })
+    }
+  })
 }
