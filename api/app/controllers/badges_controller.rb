6cf70e3f93aa169467e8b6d957872f094ed21e59
Brian Mosley & Raymond Sapida <pair+brianlmosley.raysapida@devbootcamp.com>
2016-06-22 11:30:12 -0700

    Add a post route for creating badges

--- api/app/controllers/badges_controller.rb
+++ api/app/controllers/badges_controller.rb
@@ -1,4 +1,13 @@
 class BadgesController < ApplicationController
 
+  def create
+    @teacher = Teacher.find(params[:teacher_id])
+    @badge = @teacher.badges.build( phrase: params[:phrase],teacher_id: params[:teacher_id], votes: params[:votes])
 
+    if @badge.save
+      render json: @badge
+    else
+      render json: @badge.errors.full_message
+    end
+  end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :teachers, only: [:index, :show]
+  resources :teachers, only: [:index, :show] do
+      resources :badges, only: [:create]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
